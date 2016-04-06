ea773d5651e1262fe69ee3934135354d5cd1098a
lt123 <lolzqwz@foxmail.com>
2016-04-06 17:28:29 +0800

    Initial commit

---
+++ src/main/java/com/gogo/test/T.java
@@ -0,0 +1,17 @@
+package com.gogo.test;
+
+/**
+ * Created by admin on 2016/4/6.
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println("test");
+        test();
+        System.out.println("");
+
+    }
+
+    public static void test() {
+        System.out.println("T.test");
+    }
+}
